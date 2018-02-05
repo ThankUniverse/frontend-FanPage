02870c6deaae8781bc2ccb3bc43c8b16cab34e61
Maxime Collin <maxime.collin41@gmail.com>
2018-02-05 15:15:12 +0100

    Création function panier

--- JS/main.js
+++ JS/main.js
@@ -1,20 +1,49 @@
 // function quantité de figurine + prix
 function getQtFigurine(){
-let qtFigurine = document.getElementById('figurineInput').value;
-document.getElementById('inPanier').innerHTML = qtFigurine + " Figurine(s)";
-let total = qtFigurine * 7.99;
+let qtFigurine = document.getElementById('figurineInput').value;//prend valeur de l'input
+let qtBatte = document.getElementById('batteInput').value;
+let qtDentier = document.getElementById("dentierInput").value;
+let qtTShirt = document.getElementById('tShirtInput').value;
+document.getElementById('inPanier').innerHTML = qtFigurine + " Figurine(s)";//rajoute la quantité de figurine dans panier
+let total = qtBatte * 6.99 + qtFigurine * 7.99 + qtDentier * 8.99 + qtTShirt * 5.99;
 document.getElementById("total").innerHTML = "Total: " + total;
 };
 
 //function qte batte + prix
 function getQtBatte(){
   let qtFigurine = document.getElementById('figurineInput').value;
+  let qtBatte = document.getElementById('batteInput').value;//prend valeur de l'input
+  let qtDentier = document.getElementById("dentierInput").value;
+  let qtTShirt = document.getElementById('tShirtInput').value;
+  document.getElementById('inPanier2').innerHTML = qtBatte +" Batte(s)";
+  let total = qtBatte * 6.99 + qtFigurine * 7.99 + qtDentier * 8.99 + qtTShirt * 5.99;
+  document.getElementById("total").innerHTML = "Total: " + total;
+};
+//function qte dentier + prix
+function getQtDentier(){
+  let qtFigurine = document.getElementById('figurineInput').value;
   let qtBatte = document.getElementById('batteInput').value;
-if (qtFigurine == 0) {
-  document.getElementById('inPanier').innerHTML = qtBatte +" Batte(s)";
-}
-else {
-
-  document.getElementById('inPanier').append("<\br>" + qtBatte + " Batte(s)");
-}
-}
+  let qtDentier = document.getElementById("dentierInput").value;//prend valeur de l'input
+  let qtTShirt = document.getElementById('tShirtInput').value;
+  document.getElementById("inPanier3").innerHTML = qtDentier + " Dentier(s)";
+  let total = qtBatte * 6.99 + qtFigurine * 7.99 + qtDentier * 8.99 + qtTShirt * 5.99;
+  document.getElementById("total").innerHTML = "Total: " + total;
+};
+//function qte t-shirt + prix
+function getQtTShirt(){
+  let qtFigurine = document.getElementById('figurineInput').value;
+  let qtBatte = document.getElementById('batteInput').value;
+  let qtDentier = document.getElementById("dentierInput").value;
+  let qtTShirt = document.getElementById('tShirtInput').value;
+  document.getElementById('inPanier4').innerHTML = qtTShirt + " T-shirt(s)";
+  let total = qtBatte * 6.99 + qtFigurine * 7.99 + qtDentier * 8.99 + qtTShirt * 5.99;
+  document.getElementById("total").innerHTML = "Total: " + total;
+};
+function getPrice(){
+  let qtFigurine = document.getElementById('figurineInput').value;
+  let qtBatte = document.getElementById('batteInput').value;
+  let qtDentier = document.getElementById("dentierInput").value;
+  let qtTShirt = document.getElementById('tShirtInput').value;
+  let total = qtBatte * 6.99 + qtFigurine * 7.99 + qtDentier * 8.99 + qtTShirt * 5.99;
+  alert(total);
+  }
